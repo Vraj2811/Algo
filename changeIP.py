440e169aa2bd7ebe53d0363eaa573b0963016082
Vraj Shah <vraj.shah@iitgn.ac.in>
2023-12-04 21:59:27 +0530

    commit

--- changeIP.py
+++ changeIP.py
@@ -1,23 +1,41 @@
 import os
+import re
 
-def replace_word_in_file(file_path, old_word, new_word):
+def find_first_ip_address(content):
+    # Regular expression to find an IP address in the content
+    ip_pattern = re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b')
+    match = ip_pattern.search(content)
+    if match:
+        return match.group()
+    else:
+        return None
+
+def replace_word_in_file(file_path, new_word):
     with open(file_path, 'r') as file:
         content = file.read()
 
-    content = content.replace(old_word, new_word)
+    # Find the first IP address in the content
+    old_word = find_first_ip_address(content)
 
-    with open(file_path, 'w') as file:
-        file.write(content)
+    if old_word:
+        print(f"Found IP address in {file_path}: {old_word}")
+        # Replace the old word with the new word
+        content = content.replace(old_word, new_word)
 
-def replace_word_in_directory(directory_path, old_word, new_word, file_extensions):
-    for filename in os.listdir(directory_path):
-        if filename.endswith(file_extensions):
-            file_path = os.path.join(directory_path, filename)
-            replace_word_in_file(file_path, old_word, new_word)
+        with open(file_path, 'w') as file:
+            file.write(content)
+    else:
+        print(f"No IP address found in {file_path}")
 
-directory_path = 'Vraj/htmls'
-old_word = '3.110.142.32'
-new_word = '3.110.182.250'
+def replace_word_in_directory(directory_path, new_word, file_extensions):
+    for root, dirs, files in os.walk(directory_path):
+        for filename in files:
+            if filename.endswith(file_extensions):
+                file_path = os.path.join(root, filename)
+                replace_word_in_file(file_path, new_word)
+
+directory_path = 'htmls'
+new_word = input("Enter the new word: ")
 file_extensions = (".js", ".html")
 
-replace_word_in_directory(directory_path, old_word, new_word, file_extensions)
+replace_word_in_directory(directory_path, new_word, file_extensions)
